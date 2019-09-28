5397d1739f6b2af284f4ca885ebbba740c2d7633
ryanbandrowski <50809219+ryanbandrowski@users.noreply.github.com>
2019-09-28 10:59:36 -0700

    project setup

---
+++ static/data/api_keys.py
@@ -0,0 +1,3 @@
+gkey = "AIzaSyC5-xJykekmandyy5-Dd9Xlhd8WMLUt44Y"
+
+weather_key = "5ABcuTjNZGvCEh1tMnkvz4xkBPGNgkSj"
---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoicnliYW5kMCIsImEiOiJjazBuM2xpdnowM3FqM21td3d2ejY5ejZrIn0.fu8ijgIym04OgcrNpijmTQ";
