073656de251ee364872262aa73ddb07a73943cb2
ryanbandrowski <50809219+ryanbandrowski@users.noreply.github.com>
2019-09-29 14:50:47 -0700

    added county rates db table and route

--- app.py
+++ app.py
@@ -14,10 +14,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
-#################################################
 # Database Setup
-#################################################
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///static/data/db/CA_County_Crime.sqlite"
 db = SQLAlchemy(app)
 
@@ -27,63 +24,16 @@ Base = automap_base()
 Base.prepare(db.engine, reflect=True)
 
 # save references to each table
-total_data = Base.classes.total_data
 county_data = Base.classes.county_data
+county_rates = Base.classes.county_rates
 city_data = Base.classes.city_data
 
+# Define Routes
 
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
-
-
-@app.route("/total")
-def total():
-    # Use Pandas to perform the sql query
-    sel = [
-        total_data.County,
-        total_data.County_Type,
-        total_data.Violent_Crime,
-        total_data.Murder_And_Nonnegligent_Manslaughter,
-        total_data.Forcible_Rape,
-        total_data.Robbery,
-        total_data.Aggravated_Assault,
-        total_data.Property_Crime,
-        total_data.Burglary,
-        total_data.Larceny_Theft,
-        total_data.Motor_Vehicle_Theft,
-        total_data.Total_Officers,
-        total_data.Population,
-        total_data.Land_Area
-    ]
-
-    results = db.session.query(*sel).all()
-
-    # Create a dictionary entry for each row of metadata information
-    dict_list = []
-    for result in results:
-
-        total_dict = {}
-
-        total_dict["County"] = result[0]
-        total_dict["County_Type"] = result[1]
-        total_dict["Violent_Crime"] = result[2]
-        total_dict["Murder_And_Nonnegligent_Manslaughter"] = result[3]
-        total_dict["Forcible_Rape"] = result[4]
-        total_dict["Robbery"] = result[5]
-        total_dict["Aggravated_Assault"] = result[6]
-        total_dict["Property_Crime"] = result[7]
-        total_dict["Burglary"] = result[8]
-        total_dict["Larceny_Theft"] = result[9]
-        total_dict["Motor_Vehicle_Theft"] = result[10]
-        total_dict["Total_Officers"] = result[11]
-        total_dict["Population"] = result[12]
-        total_dict["Land_Area"] = result[13]
-        
-        dict_list.append(total_dict)
-
-    return jsonify(dict_list)
 
 
 @app.route("/county")
@@ -94,6 +44,7 @@ def county():
         county_data.County_Type,
         county_data.Lat,
         county_data.Lng,
+        county_data.Total_Crime,
         county_data.Violent_Crime,
         county_data.Murder_And_Nonnegligent_Manslaughter,
         county_data.Forcible_Rape,
@@ -102,7 +53,10 @@ def county():
         county_data.Property_Crime,
         county_data.Burglary,
         county_data.Larceny_Theft,
-        county_data.Motor_Vehicle_Theft
+        county_data.Motor_Vehicle_Theft,
+        county_data.Total_Officers,
+        county_data.Population,
+        county_data.Land_Area
     ]
 
     results = db.session.query(*sel).all()
@@ -117,17 +71,65 @@ def county():
         county_dict["County_Type"] = result[1]
         county_dict["Lat"] = result[2]
         county_dict["Lng"] = result[3]
-        county_dict["Violent_Crime"] = result[4]
-        county_dict["Murder_And_Nonnegligent_Manslaughter"] = result[5]
-        county_dict["Forcible_Rape"] = result[6]
-        county_dict["Robbery"] = result[7]
-        county_dict["Aggravated_Assault"] = result[8]
-        county_dict["Property_Crime"] = result[9]
-        county_dict["Burglary"] = result[10]
-        county_dict["Larceny_Theft"] = result[11]
-        county_dict["Motor_Vehicle_Theft"] = result[12]
+        county_dict["Total_Crime"] = result[4]
+        county_dict["Violent_Crime"] = result[5]
+        county_dict["Murder_And_Nonnegligent_Manslaughter"] = result[6]
+        county_dict["Forcible_Rape"] = result[7]
+        county_dict["Robbery"] = result[8]
+        county_dict["Aggravated_Assault"] = result[9]
+        county_dict["Property_Crime"] = result[10]
+        county_dict["Burglary"] = result[11]
+        county_dict["Larceny_Theft"] = result[12]
+        county_dict["Motor_Vehicle_Theft"] = result[13]
+        county_dict["Total_Officers"] = result[14]
+        county_dict["Population"] = result[15]
+        county_dict["Land_Area"] = result[16]
         
         dict_list.append(county_dict)
+
+    return jsonify(dict_list)
+
+
+@app.route("/county_rates")
+def countyRates():
+    # Use Pandas to perform the sql query
+    sel = [
+        county_rates.County,
+        county_rates.County_Type,
+        county_rates.Total_Crime,
+        county_rates.Violent_Crime,
+        county_rates.Murder_And_Nonnegligent_Manslaughter,
+        county_rates.Forcible_Rape,
+        county_rates.Robbery,
+        county_rates.Aggravated_Assault,
+        county_rates.Property_Crime,
+        county_rates.Burglary,
+        county_rates.Larceny_Theft,
+        county_rates.Motor_Vehicle_Theft
+    ]
+
+    results = db.session.query(*sel).all()
+
+    # Create a dictionary entry for each row of metadata information
+    dict_list = []
+    for result in results:
+
+        county_rates_dict = {}
+
+        county_rates_dict["County"] = result[0]
+        county_rates_dict["County_Type"] = result[1]
+        county_rates_dict["Total_Crime"] = result[2]
+        county_rates_dict["Violent_Crime"] = result[3]
+        county_rates_dict["Murder_And_Nonnegligent_Manslaughter"] = result[4]
+        county_rates_dict["Forcible_Rape"] = result[5]
+        county_rates_dict["Robbery"] = result[6]
+        county_rates_dict["Aggravated_Assault"] = result[7]
+        county_rates_dict["Property_Crime"] = result[8]
+        county_rates_dict["Burglary"] = result[9]
+        county_rates_dict["Larceny_Theft"] = result[10]
+        county_rates_dict["Motor_Vehicle_Theft"] = result[11]
+        
+        dict_list.append(county_rates_dict)
 
     return jsonify(dict_list)
 
