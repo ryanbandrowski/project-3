d23ec0d1fde6b36e4a4c3d529e87659d4d842678
ryanbandrowski <50809219+ryanbandrowski@users.noreply.github.com>
2019-10-04 18:11:26 -0700

    city markers done, county popup in progress

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,4 @@
-// Create the tile layer that will be the background of our map
+
 var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
   maxZoom: 18,
@@ -6,64 +6,104 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/til
   accessToken: API_KEY
 });
 
-// Create a baseMaps object to hold the lightmap layer
+
 var baseMaps = {
   "Light Map": lightmap
 };
 
-// Create the map object with options
+
 var map = L.map("map-id", {
   center: [36.7783, -119.4179],
   zoom: 6,
   layers: [lightmap]
 });
 
-// Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
-// L.control.layers(baseMaps, {
-//   collapsed: false
-// }).addTo(map);
+
 
 var link = 'https://opendata.arcgis.com/datasets/f26ff0ccda6e48a8b923927cfdb2a451_0.geojson'
-
 var geoJson;
 
-// Grabbing our GeoJSON data..
 d3.json(link, function(data) {
-    console.log(data)
-    // Creating a geoJSON layer with the retrieved data
-    geoJson = L.geoJson(data, {
-        // Style for each feature (in this case a neighborhood)
-        style: function(feature) {
-            return {
-                color: "lightgrey",
-                // Call the chooseColor function to decide which color to color our neighborhood (color based on borough)
-                fillColor: "green",
-                fillOpacity: 0.5,
-                weight: 1
-            };
-        },
-        // Called on each feature
-        onEachFeature: function(feature, layer) {
-        // Setting various mouse events to change style when different events occur
-            layer.on({
-                // On mouse over, make the feature (neighborhood) more visible
-                mouseover: function(event) {
-                    layer = event.target;
-                    layer.setStyle({
-                        fillOpacity: 0.9
-                    });
-                },
-                // Set the features style back to the way it was
-                mouseout: function(event) {
-                    geoJson.resetStyle(event.target);
-                },
-                // When a feature (neighborhood) is clicked, fit that feature to the screen
-                click: function(event) {
-                    map.fitBounds(event.target.getBounds());
-                }
-            });
-        // Giving each feature a pop-up with information about that specific feature
-        layer.bindPopup("<h1>" + feature.properties.COUNTY_NAME + "</h1>");
-        }
-    }).addTo(map);
+
+    d3.json("/county_rates", function(county) {
+
+        geoJson = L.geoJson(data, {
+
+            style: function(feature) {
+                return {
+                    color: "lightgrey",
+                    fillColor: "green",
+                    fillOpacity: .2,
+                    weight: 1
+                };
+            },
+
+            onEachFeature: function(feature, layer) {
+
+                layer.on({
+
+                    mouseover: function(event) {
+                        layer = event.target;
+                        layer.setStyle({
+                            fillOpacity: 0.7
+                        });
+                    },
+
+                    mouseout: function(event) {
+                        geoJson.resetStyle(event.target);
+                    },
+
+                    click: function(event) {
+                        map.fitBounds(event.target.getBounds());
+                    }
+                });
+
+                layer.bindPopup(
+                    "<h2>" + county.County + " Crime Rates</h2>" +
+                    "<h3>Per 10,000 People</h3>" +
+                    "<strong>Total Crime: " + county.Total_Crime
+                );
+            }
+        }).addTo(map);
+    });
+});
+
+
+
+var markers = L.layerGroup();
+
+d3.json("/city", function(response) {
+    console.log(response);
+
+    for (var i = 0; i < response.length; i++) {
+        
+        var lat = response[i].Lat
+        var lng = response[i].Lng
+
+        markers.addLayer(L.marker([lat,lng])
+            .bindPopup(
+                "<strong>City: </strong>" + response[i].City + "<br>" +
+                "<strong>County: </strong>" + response[i].County + "<br><br>" +
+                "<strong>Violent Crime: </strong>" + response[i].Violent_Crime + "<br>" +
+                "<strong>Murder/Manslaughter: </strong>" + response[i].Murder_And_Nonnegligent_Manslaughter + "<br>" +
+                "<strong>Forcible Rape: </strong>" + response[i].Forcible_Rape + "<br>" +
+                "<strong>Robbery: </strong>" + response[i].Robbery + "<br>" +
+                "<strong>Aggravated Assault: </strong>" + response[i].Aggravated_Assault + "<br><br>" +
+                "<strong>Property Crime: </strong>" + response[i].Property_Crime + "<br>" +
+                "<strong>Burglary: </strong>" + response[i].Burglary + "<br>" +
+                "<strong>Larceny-Theft: </strong>" + response[i].Larceny_Theft + "<br>" +
+                "<strong>Motor Vehicle Theft: </strong>" + response[i].Motor_Vehicle_Theft + "<br><br>" +
+                "<strong>Total Officers: </strong>" + response[i].Total_Officers + "<br>"
+            )
+        );
+    }
+});
+
+map.on('zoomend', function() {
+    if (map.getZoom() <9){
+            map.removeLayer(markers);
+    }
+    else {
+            map.addLayer(markers);
+    }
 });
